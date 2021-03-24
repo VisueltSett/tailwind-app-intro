d9a860a222c3d06a671bced9f2393d05c3b9ff02
VisueltSett_AneTh <ane@visueltsett.no>
2021-03-24 11:49:18 +0100

    adding initial files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'card-heading': '#0e1726'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
